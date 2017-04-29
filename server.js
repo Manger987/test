ae3c913d81dbb9b3550974553a0c5203933eee88
German Poblete <mangerpv@gmail.com>
2017-04-29 15:16:21 -0300

    Agregado index.html

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var http = require('http');
+var fs = require('fs');
 var server = http.createServer();
+
+var html = fs.readFileSync("./index.html");
 function control(petic, resp) {
-resp.writeHead(200, {'content-type': 'text/plain'});
-resp.write('Hola, Mundo!');
-resp.end();
+    resp.writeHead(200, {'content-type': 'text/html'});
+    resp.write(html);
+    //resp.render();
+    //resp.write('Hola, Mundo !');
+    resp.end();
 }
 server.on('request', control);
 server.listen(8080);
